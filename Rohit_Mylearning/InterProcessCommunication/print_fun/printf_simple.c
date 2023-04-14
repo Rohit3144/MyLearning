828e096777fa3682a2bdabb6d53421c61fcc689a
Rohit kumar <rohit.kumar@dixoninfo.com>
2023-04-14 14:32:22 +0530

    updates

---
+++ Rohit_Mylearning/InterProcessCommunication/print_fun/printf_simple.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int i =10;
+    i = printf("%d\n", i);
+    i = printf("Rohit...Hello world\n");
+
+    //It will count the number of characters in string then print
+    printf("%d\n", i); 
+
+    return 0;
+}
