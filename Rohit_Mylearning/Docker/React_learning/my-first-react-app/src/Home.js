22940d2dfff6412ffba5437fe3733c3594473734
Rohit kumar <rohit.kumar@dixoninfo.com>
2023-08-18 16:24:31 +0530

    Added react files

---
+++ Rohit_Mylearning/Docker/React_learning/my-first-react-app/src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';  
+
+export default class Home extends React.Component
+{
+    render()
+    {
+        return 
+        (
+            <div>
+            <h1>This is home component</h1>
+            <h2>Print it</h2>
+            </div>
+        );
+    }
+}
