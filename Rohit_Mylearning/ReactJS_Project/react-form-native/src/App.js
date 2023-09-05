af5dbac86e4ad3bedac0359c4c1b5f9de864e870
Rohit Kumar <rohit.kumar@dixoninfo.com>
2023-09-05 12:41:32 +0530

    Added onChange functionality

--- Rohit_Mylearning/ReactJS_Project/react-form-native/src/App.js
+++ Rohit_Mylearning/ReactJS_Project/react-form-native/src/App.js
@@ -1,6 +1,26 @@
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const initialValue = { username: "", email: "", password: "" };
+  const [formValues, setFormValues] = useState(initialValue);
+
+  // console.log("formValue = ", formValues);
+
+  // let count = 1;
+  // const handleChange = () => {
+  //   console.log("Rohit Kuhar", count++);
+  // }
+
+  function handleChange(e) {
+    // console.log("Rohit Kuhar ==> ", e.target.value);
+    const { name, value } = e.target;
+    // console.log("name => ", name);
+    // console.log("value => ", value);
+    setFormValues({ ...formValues, [name]: value });
+    // console.log("formValues => ", formValues);
+  }
+
   return (
     <div className="container">
       <form>
@@ -9,17 +29,35 @@ function App() {
         <div className="ui form">
           <div className="field">
             <label>username</label>
-            <input type="text" name="username" placeholder="your name">
+            <input
+              type="text"
+              name="username"
+              placeholder="your name"
+              value={formValues.username}
+              onChange={handleChange}
+            >
             </input>
           </div>
           <div className="field">
             <label>email</label>
-            <input type="email" name="email" placeholder="your email">
+            <input
+              type="email"
+              name="email"
+              placeholder="your email"
+              value={formValues.email}
+              onChange={handleChange}
+            >
             </input>
           </div>
           <div className="field">
             <label>password</label>
-            <input type="password" name="password" placeholder="password">
+            <input
+              type="password"
+              name="password"
+              placeholder="password"
+              value={formValues.password}
+              onChange={handleChange}
+            >
             </input>
           </div>
           <button className="fluid ui button Black">Submit</button>
